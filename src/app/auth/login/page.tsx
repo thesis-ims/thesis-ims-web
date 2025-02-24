d75e1785ef523dc73daeec7425dd679f5745e3d3
willySprout <william@sprout.co.id>
2025-02-24 11:17:35 +0700

    create a simple validation if the field is empty

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,16 +1,25 @@
 "use client";
 
 import { loginSchema } from "@/utils/zodValidations";
+import { error } from "console";
 import { useState } from "react";
+import { ZodIssueBase } from "zod";
 
 interface FormDataProps {
   email: string;
   password: string;
 }
 
+interface FormDataErrorProps {
+  path: string;
+  message: string;
+}
+
 function Login() {
   const [formData, setFormData] = useState<FormDataProps>({} as FormDataProps);
-  const [error, setError] = useState<any>({});
+  const [errors, setErrors] = useState<FormDataErrorProps[]>(
+    [] as FormDataErrorProps[]
+  );
 
   function handleOnChangeInput(e: React.ChangeEvent<HTMLInputElement>) {
     const { name, value } = e.target;
@@ -21,18 +30,36 @@ function Login() {
     const validationResult = loginSchema.safeParse(formData);
     console.log(validationResult, "validate");
 
-    if (validationResult.success) {
-      console.log("success parse");
-    } else {
-      console.log("failed parse");
-      console.log(validationResult.error.errors);
+    if (!validationResult.success) {
+      const issues: FormDataErrorProps[] = validationResult.error.issues.map(
+        (issue) => {
+          return {
+            path: issue.path[0] as string,
+            message: issue.message,
+          };
+        }
+      );
+      setErrors(issues);
+      return;
     }
+    setErrors([]);
+    alert("continue to login flow");
+  }
+
+  function getZodErrorMessage(path: string) {
+    const errorMessages = errors.map((error) => {
+      if (path === error.path) {
+        return error.message as string;
+      }
+    });
+    return errorMessages;
   }
 
   return (
     <div className="flex flex-col gap-1">
       <h1>Login Page</h1>
 
+      {JSON.stringify(errors)}
       <input
         name="email"
         type="text"
@@ -40,6 +67,7 @@ function Login() {
         value={formData.email}
         onChange={handleOnChangeInput}
       />
+      <p>{getZodErrorMessage("email")}</p>
 
       <input
         name="password"
@@ -48,6 +76,8 @@ function Login() {
         value={formData.password}
         onChange={handleOnChangeInput}
       />
+
+      <p>{getZodErrorMessage("password")}</p>
 
       <button className="bg-red-500 w-20 h-10" onClick={handleSubmitLoginForm}>
         submit
