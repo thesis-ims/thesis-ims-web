66ad54a8c8df6c628f1b8e5ea9db0c4b7fbddaba
William Amadeus <williamama34@gmail.com>
2025-05-01 20:36:33 +0700

    redirects user to login page if user has no session

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -5,9 +5,13 @@ import Image from "next/image";
 import { getSession } from "@/lib/auth/get-session";
 import { ProfileProps } from "@/interfaces/profile";
 import Homepage from "../homepage";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const profile = await getUserProfile();
+  if (profile.error || Object.keys(profile.data).length === 0) {
+    redirect("/auth/login");
+  }
 
   return <Homepage profile={profile.data} />;
 }
