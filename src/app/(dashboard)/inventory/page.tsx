bfb1e2d96b00c67b213bfee47ed1cc1179e31e58
William Amadeus <williamama34@gmail.com>
2025-05-15 01:20:49 +0700

    on progress stock information section

--- src/app/(dashboard)/inventory/page.tsx
+++ src/app/(dashboard)/inventory/page.tsx
@@ -1,5 +1,6 @@
 import ProductListTable from "./components/product-list-table";
 import { getAllProducts } from "@/lib/api/product";
+import StockInformation from "./components/stock-information";
 
 export default async function Inventory() {
   const products = await getAllProducts();
@@ -7,7 +8,7 @@ export default async function Inventory() {
     <div className="flex flex-col gap-6">
       <h1 className="text-4xl font-bold">Inventory</h1>
       {/* {JSON.stringify(products.data)} */}
-      <div>stocks section</div>
+      <StockInformation />
       <ProductListTable products={products.data.object} />
     </div>
   );
