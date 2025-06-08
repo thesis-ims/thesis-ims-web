1f48d950e0bedfedb8df8cd4539cdb664790e98a
William Amadeus <williamama34@gmail.com>
2025-06-08 17:07:38 +0700

    revamp category field on product form

--- src/app/(dashboard)/add-product/page.tsx
+++ src/app/(dashboard)/add-product/page.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import AddProductForm from "../inventory/components/add-product-form";
 import PageHeader from "@/components/ui/page-header";
+import { getCategorySummary } from "@/lib/api/product";
 
-export default function AddProduct() {
+export default async function AddProduct() {
+  const categorySummary = await getCategorySummary();
+  const categoryList = categorySummary.data.map((category) => category.label);
   return (
     <div className="flex flex-col gap-6">
       <PageHeader title="Add Product" showBackIcon={true} />
-      <AddProductForm />
+      <AddProductForm categoryList={categoryList} />
     </div>
   );
 }
---
+++ src/components/ui/category-combobox.tsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from "react";
+import {
+  Combobox,
+  ComboboxButton,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+} from "@headlessui/react";
+import { ChevronDownIcon } from "lucide-react";
+import { cn } from "@/utils/tw-merge";
+
+export default function CategoryCombobox({
+  categoryList,
+  errorMessages,
+  helperText,
+  onChange,
+  initValue,
+}: {
+  categoryList: string[];
+  errorMessages?: string;
+  helperText?: string;
+  onChange: (value: string) => void;
+  initValue: string;
+}) {
+  const [selectedValue, setSelectedValue] = useState("");
+  const [query, setQuery] = useState("");
+  const filteredValue =
+    query === ""
+      ? categoryList
+      : categoryList.filter((category) => {
+          return category.toLowerCase().includes(query.toLowerCase());
+        });
+
+  function setQueryAndValue(query: string) {
+    setQuery(query);
+    setSelectedValue(query);
+    onChange(query!);
+  }
+
+  useEffect(() => {
+    setSelectedValue(initValue);
+  }, [initValue]);
+
+  return (
+    <div className="flex flex-col gap-1">
+      <Combobox
+        value={selectedValue}
+        onChange={(value) => {
+          setSelectedValue(value!);
+          onChange(value!);
+        }}
+        onClose={() => setQuery("")}
+      >
+        <div className="relative">
+          <ComboboxInput
+            className={cn(
+              "bg-gray-10 w-full px-4 py-3 focus:outline-0",
+              errorMessages
+                ? "border-2 border-red-600"
+                : "border-b border-[#C1C7CD]",
+            )}
+            placeholder="Enter product category"
+            aria-label="Categories"
+            displayValue={(value) => value as string}
+            onChange={(event) => setQueryAndValue(event.target.value)}
+          />
+          <ComboboxButton className="group absolute inset-y-0 right-0 cursor-pointer px-2.5">
+            <ChevronDownIcon className="text-gray-60 h-6 w-6" />
+          </ComboboxButton>
+        </div>
+
+        <ComboboxOptions
+          anchor="bottom"
+          className="w-(--input-width) shadow-lg"
+        >
+          {filteredValue.map((value, index) => (
+            <ComboboxOption
+              key={index}
+              value={value}
+              className={`bg-gray-10 hover:bg-gray-20 cursor-pointer px-3 py-2 ${value === selectedValue ? "bg-gray-20" : ""}`}
+            >
+              {value}
+            </ComboboxOption>
+          ))}
+        </ComboboxOptions>
+      </Combobox>
+
+      {helperText && !errorMessages && (
+        <p className="text-gray-60 text-xs">{helperText}</p>
+      )}
+
+      {errorMessages && (
+        <p className="text-xs font-medium text-red-600">{errorMessages}</p>
+      )}
+    </div>
+  );
+}
